911bb7d1eded57cc76b84eb7720802f7197880ec
vara-s830 <svaralakshmi090@gmail.com>
2025-06-02 11:58:20 +0530

    Add files via upload

---
+++ book review-mvp/book response (DTO).java
@@ -0,0 +1,59 @@
+
+public class BookResponse {
+
+    private String id;
+    private String title;
+    private String author;
+    private String description;
+    private String thumbnailUrl;
+    private float rating;
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+}
